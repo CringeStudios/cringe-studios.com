9958c6e872331a7bfa32311a13b748c7536a95fc
MrLetsplay <mr@cringe-studios.com>
2024-03-08 19:51:51 +0100

    April fools

---
+++ special/april-fools.js
@@ -0,0 +1,32 @@
+let websiteScale = 1;
+let OVERRIDE_APRIL_FOOLS = false;
+
+setInterval(() => {
+	const theTime = new Date();
+	if (!OVERRIDE_APRIL_FOOLS && (theTime.getDate() != 1 || theTime.getMonth() != 3)) {
+		document.body.style.transition = undefined;
+		document.body.style.transform = undefined;
+		return;
+	}
+
+	document.body.style.transition = "all linear 0.1s";
+	document.body.style.transform = "scale(" + websiteScale + ")";
+	websiteScale *= 0.95;
+}, 100);
+
+let oldMouseX, oldMouseY;
+let oldMouseTime;
+
+document.addEventListener("mousemove", (ev) => {
+	let time = new Date().getTime();
+
+	if(oldMouseX !== undefined || oldMouseY !== undefined) {
+		let dist = Math.sqrt(Math.pow(oldMouseX - ev.x, 2) + Math.pow(oldMouseY - ev.y, 2));
+		let velocity = dist / (time - oldMouseTime);
+		websiteScale = Math.min(1, websiteScale * 1 + velocity/750);
+	}
+
+	oldMouseX = ev.x;
+	oldMouseY = ev.y;
+	oldMouseTime = time;
+})
